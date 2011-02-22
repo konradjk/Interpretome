2a0caf1c34bcb631cfbbe7c31f49746615b0bbd3
gene210 <stanford.gene210@gmail.com>
2011-02-22 02:16:38 -0800

    Include files.

---
+++ media/js/template.js
@@ -0,0 +1,12 @@
+<script id="snp-tmpl" type="text/x-jquery-tmpl">
+  <tr><td>${dbsnp}</td><td>${genotype}</td></tr>
+</script>
+    
+<script id="chromosome-tmpl" type="text/x-jquery-tmpl">
+  <tr><td>${chromosome}</td><td>${count}</td></tr>
+</script>
+<script id="status-tmpl" type="text/x-jquery-tmpl">
+  Loaded ${count} SNPs.
+  <br />
+  <b><a href="">Clear my file</a></b>
+</script>
