bdfca3333e516d5ebdb5cfea4f85b4dda06ee756
gene210 <stanford.gene210@gmail.com>
2011-02-22 02:15:47 -0800

    Work on imputation.

--- application/views.py
+++ application/views.py
@@ -14,19 +14,42 @@ def dict_cursor(self):
   cursor.close()
   return base.CursorWrapper(self.connection.cursor(MySQLdb.cursors.DictCursor))
 base.DatabaseWrapper.dict_cursor = dict_cursor
+
+def strip_rsids(params):
+  if 'dbsnp' in params:
+    pass
   
 
 def linked(request):
-  cursor = connections['default'].dict_cursor()
   dbsnp = request.GET.get('dbsnp', None)
-  cursor.execute(r"SELECT * FROM var_ld_data.ld_CEU LIMIT 1;")
+  population = request.GET.get('population', None)
+  if dbsnp is None or population is None:
+    return http.HttpResponseBadRequest()
+  
+  dbsnp = int(dbsnp.lstrip('rs'))
+  cursor = connections['default'].dict_cursor()
+  
+  cursor.execute('''
+    SELECT dbSNP1, dbSNP2, R_square 
+    FROM var_ld_data.ld_%s 
+    WHERE dbSNP1 = %d OR dbSNP2 = %d
+    ORDER BY R_square DESC;''' % (population, dbsnp, dbsnp)
+  )
   result = cursor.fetchall()
   
   return http.HttpResponse(
     simplejson.dumps(result),
-    mimetype = "application/json"
+    mimetype = 'application/json'
   )
   
+def impute(request):
+  dbsnp = request.GET.get('dbsnp', None)
+  if dbsnp is None:
+    pass
+  
+  dbsnp = int(dbsnp.lstrip('rs'))
+  return http.HttpResponse(simplejson.dumps("Success"), mimetype = "application/json")
+    
 
 def index(request):
 	return shortcuts.render_to_response('application/index.html', {})
